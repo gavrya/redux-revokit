6a9c4daf85312ec54653a0311dbc9d0242ac8c36
Oleksandr <gavrya@gmail.com>
2023-08-22 13:05:49 +0300

    improvements

--- build/hotTopics.d.ts
+++ build/hotTopics.d.ts
@@ -1,9 +1,8 @@
-import type { createTopicMiddleware } from './topicMiddleware';
-import type { Topic } from './types/topicMiddleware';
+import type { Topic, TopicMiddleware } from './types/topicMiddleware';
 import type { Store } from 'redux';
 declare const TOPIC_INJECTED = "@topicMiddleware/TOPIC_INJECTED";
 declare const TOPIC_EJECTED = "@topicMiddleware/TOPIC_EJECTED";
-declare const hotTopics: (store: Store, topicMiddleware: ReturnType<typeof createTopicMiddleware>) => {
+declare const hotTopics: (store: Store, topicMiddleware: TopicMiddleware) => {
     injectTopic: (name: string, topic: Topic) => void;
     ejectTopic: (name: string) => void;
 };
--- build/index.d.ts
+++ build/index.d.ts
@@ -4,4 +4,4 @@ export { hotTopics, TOPIC_INJECTED, TOPIC_EJECTED } from './hotTopics';
 export { runTopic, combineTopics, createTopicMiddleware, } from './topicMiddleware';
 export { ofType, createSwitchEffect } from './utils';
 export type { ObjectValuesUnion, ActionsFromActionCreators, } from './types/actionGenerator';
-export type { TopicProps, Topic, Effect } from './types/topicMiddleware';
+export type { TopicProps, Topic, Effect, TopicMiddleware, } from './types/topicMiddleware';
